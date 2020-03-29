78a24d35ae6c472b27a61a145ca0b96e8f940b07
AlessiaGuzzo <61712651+AlessiaGuzzo@users.noreply.github.com>
2020-03-29 17:44:04 +0200

    Creation of classes: Deck, God, Power

---
+++ src/main/java/it/polimi/ingsw/PSP30/Deck.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.PSP30;
+import java.util.ArrayList;
+
+/**
+ * @author Alessia Guzzo
+ */
+
+public class Deck {
+    /**
+     * This class uses java.util.ArrayList for implementing the ArrayList which
+     * includes all the types of GodCard that can be used during the game session
+     */
+    private ArrayList<God> cardList;
+
+
+    public Deck(ArrayList<God> cardList) {
+        this.cardList = cardList;
+    }
+
+
+    public ArrayList<God> getCardList() {
+        return cardList;
+    }
+
+
+    public void setCardList(ArrayList<God> cardList) {
+        this.cardList = cardList;
+    }
+}
+
--- src/main/java/it/polimi/ingsw/PSP30/Main.java
+++
@@ -1,4 +0,0 @@
-package it.polimi.ingsw.PSP30;
-
-public class Main {
-}
