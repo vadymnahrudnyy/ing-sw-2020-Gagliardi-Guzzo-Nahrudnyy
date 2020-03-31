4026d7d862e6a9d177b80f8bcf5b95a888dd194f
Alessia-G <alessiagagliardi41@gmail.com>
2020-03-31 14:04:53 +0200

    Class Worker modified and test files created

---
+++ src/test/java/Model/PlayerTest.java
@@ -0,0 +1,52 @@
+package Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getUsername() {
+    }
+
+    @Test
+    public void getUserID() {
+    }
+
+    @Test
+    public void getWorkers() {
+    }
+
+    @Test
+    public void getworker() {
+    }
+
+    @Test
+    public void setWorkers() {
+    }
+
+    @Test
+    public void isHasMovedWorker() {
+    }
+
+    @Test
+    public void setHasMovedWorker() {
+    }
+
+    @Test
+    public void isHasBuilt() {
+    }
+
+    @Test
+    public void setHasBuilt() {
+    }
+
+    @Test
+    public void getGod() {
+    }
+
+    @Test
+    public void setGod() {
+    }
+}
