b096759800400e9acfcabad8b66e23778740ddc1
AlessiaGuzzo <61712651+AlessiaGuzzo@users.noreply.github.com>
2020-04-08 00:53:56 +0200

    Power variables changed and tests refined

--- src/main/java/Model/Power.java
+++ src/main/java/Model/Power.java
@@ -1,49 +1,47 @@
 package Model;
 
-
 /**
  * @author Alessia Guzzo
  */
 
 public class Power {
     /**
-     * This class describes the powers used in the game which are associated with Gods
+     * Power class describes the usage of the specific power
      */
 
+    private final int powerID;
     private Boolean isActive;
-    private Boolean isPlayerTurn;
-    private Boolean isOpponent;
+    private Boolean usableOnPlayerTurn;
+    private Boolean validOnOpponentTurn;
     private TurnPhase turnPhase;
 
+
     /**
-     * @param isActive states if the power have to be activated by the player, so he decides to use it
-     * @param isPlayerTurn if '1' is the player's turn
-     * @param isOpponent flag that states if the power can be activated during other's players turn
+     * @param isActive states if the power have to be activated by the player
+     * @param usableOnPlayerTurn indicates if the power can be used during the player's turn
+     * @param validOnOpponentTurn flag that states if the power can be activated during the opponent's turn
      * @param turnPhase is an enum that indicates in what phase of the game the power can be used
      */
-    public Power(Boolean isActive, Boolean isPlayerTurn, Boolean isOpponent, TurnPhase turnPhase) {
+    public Power(int powerID, Boolean isActive, Boolean usableOnPlayerTurn, Boolean validOnOpponentTurn, TurnPhase turnPhase) {
 
+        this.powerID= powerID;
         this.isActive = isActive;
-        this.isPlayerTurn = isPlayerTurn;
-        this.isOpponent = isOpponent;
+        this.usableOnPlayerTurn = usableOnPlayerTurn;
+        this.validOnOpponentTurn = validOnOpponentTurn;
         this.turnPhase = turnPhase;
     }
 
+    public int getPowerID() {
+        return powerID;
+    }
 
     public Boolean getActive() {
         return isActive;
     }
 
+    public Boolean getUsableOnPlayerTurn() { return usableOnPlayerTurn; }
 
-    public Boolean getPlayerTurn() {
-        return isPlayerTurn;
-    }
-
-
-    public Boolean getOpponent() {
-        return isOpponent;
-    }
-
+    public Boolean getValidOnOpponentTurn() { return validOnOpponentTurn; }
 
     public TurnPhase getTurnPhase() {
         return turnPhase;
