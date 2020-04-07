406cf4a343060154721690222a47ba10563784e2
Alessia-G <alessiagagliardi41@gmail.com>
2020-04-07 22:14:51 +0200

    Test and exception implemeted

--- src/test/java/Model/PlayerTest.java
+++ src/test/java/Model/PlayerTest.java
@@ -8,45 +8,138 @@ public class PlayerTest {
 
     @Test
     public void getUsername() {
+        Power[] power= new Power[1];
+        power[0] = new Power(true, true, false, TurnPhase.MOVE);
+        God testGod = new God(8, "Minotauro", 1, 3, "il mostro dalla testa di toro", power);
+        Space currentPosition = new Space(4,3,4);
+        Worker[] testworker=new Worker[1];
+        testworker[0] = new Worker(30, "Tom", 'm', currentPosition);
+        Player testPlayer= new Player("Tom", 30, testworker, testGod );
+        assertEquals("Tom",testPlayer.getUsername());
     }
 
     @Test
     public void getUserID() {
+        Power[] power= new Power[1];
+        power[0] = new Power(true, true, false, TurnPhase.MOVE);
+        God testGod = new God(8, "Minotauro", 1, 3, "il mostro dalla testa di toro", power);
+        Space currentPosition = new Space(4,3,4);
+        Worker[] testworker=new Worker[1];
+        testworker[0] = new Worker(30, "Tom", 'm', currentPosition);
+        Player testPlayer= new Player("Tom", 30, testworker, testGod );
+        assertEquals(30,testPlayer.getUserID());
     }
 
     @Test
     public void getWorkers() {
+        Space currentPosition1 = new Space(4,3,4);
+        Space currentPosition2 = new Space(2,3,4);
+        Worker[] testWorkers=new Worker[2];
+        testWorkers[0] = new Worker(30, "Player1", 'm', currentPosition1);
+        testWorkers[1] = new Worker(25, "Player1", 'm', currentPosition2);
+        Player testPlayer= new Player("Player1", 30, testWorkers, null );
+        assertArrayEquals(testWorkers,testPlayer.getWorkers());
     }
 
     @Test
-    public void getworker() {
+    public void getWorker() {
+
+        Space currentPosition1 = new Space(4,3,4);
+        Space currentPosition2 = new Space(2,3,4);
+        Worker[] testWorkers=new Worker[2];
+        testWorkers[0] = new Worker(30, "Player1", 'm', currentPosition1);
+        testWorkers[1] = new Worker(25, "Player1", 'm', currentPosition2);
+        Player testPlayer= new Player("Player1", 30, testWorkers, null );
+        assertEquals(testWorkers[0],testPlayer.getWorker(0));
+
     }
 
     @Test
     public void setWorkers() {
+        Space currentPosition = new Space(4,3,4);
+        Player testPlayer = new Player("test",3,null,null);
+        Worker[] testworker=new Worker[1];
+        testworker[0] = new Worker(30, "Tom", 'm', currentPosition);
+        testPlayer.setWorkers(testworker);
+        assertArrayEquals(testworker,testPlayer.getWorkers());
     }
 
     @Test
     public void isHasMovedWorker() {
+        Power[] power = new Power[1];
+        power[0] = new Power(true, true, false, TurnPhase.MOVE);
+        God testGod = new God(8, "Minotauro", 1, 3, "il mostro dalla testa di toro", power);
+        Space currentPosition = new Space(4, 3, 4);
+        Worker[] testworker = new Worker[1];
+        testworker[0] = new Worker(30, "Tom", 'm', currentPosition);
+        Player testPlayer = new Player("Tom", 30, testworker, testGod);
+        testPlayer.setHasMovedWorker(true);
+        assertTrue(testPlayer.isHasMovedWorker());
     }
 
     @Test
     public void setHasMovedWorker() {
+        Power[] power = new Power[1];
+        power[0] = new Power(true, true, false, TurnPhase.MOVE);
+        God testGod = new God(8, "Minotauro", 1, 3, "il mostro dalla testa di toro", power);
+        Space currentPosition = new Space(4, 3, 4);
+        Worker[] testworker = new Worker[1];
+        testworker[0] = new Worker(30, "Tom", 'm', currentPosition);
+        Player testPlayer = new Player("Tom", 30, testworker, testGod);
+        testPlayer.setHasMovedWorker(true);
+        assertTrue(testPlayer.isHasMovedWorker());
     }
 
     @Test
     public void isHasBuilt() {
+        Power[] power = new Power[1];
+        power[0] = new Power(true, true, false, TurnPhase.MOVE);
+        God testGod = new God(8, "Minotauro", 1, 3, "il mostro dalla testa di toro", power);
+        Space currentPosition = new Space(4, 3, 4);
+        Worker[] testworker = new Worker[1];
+        testworker[0] = new Worker(30, "Tom", 'm', currentPosition);
+        Player testPlayer = new Player("Tom", 30, testworker, testGod);
+        testPlayer.setHasBuilt(true);
+        assertTrue(testPlayer.isHasBuilt());
     }
 
     @Test
     public void setHasBuilt() {
+        Power[] power = new Power[1];
+        power[0] = new Power(true, true, false, TurnPhase.MOVE);
+        God testGod = new God(8, "Minotauro", 1, 3, "il mostro dalla testa di toro", power);
+        Space currentPosition = new Space(4, 3, 4);
+        Worker[] testworker = new Worker[1];
+        testworker[0] = new Worker(30, "Tom", 'm', currentPosition);
+        Player testPlayer = new Player("Tom", 30, testworker, testGod);
+        testPlayer.setHasBuilt(true);
+        assertTrue(testPlayer.isHasBuilt());
     }
 
     @Test
     public void getGod() {
+        Power[] power= new Power[1];
+        power[0] = new Power(true, true, false, TurnPhase.MOVE);
+        God testGod = new God(8, null, 1, 3, "il mostro dalla testa di toro", power);
+        Space currentPosition = new Space(4,3,4);
+        Worker[] testworker=new Worker[1];
+        testworker[0] = new Worker(30, "Tom", 'm', currentPosition);
+        Player testPlayer= new Player("Tom", 30, testworker, testGod );
+        assertEquals(testGod, testPlayer.getGod());
     }
 
     @Test
     public void setGod() {
+        Power[] power= new Power[1];
+        power[0] = new Power(true, true, false, TurnPhase.MOVE);
+        God testGod = new God(8, null, 1, 3, "il mostro dalla testa di toro", power);
+        Space currentPosition = new Space(4,3,4);
+        Worker[] testworker=new Worker[1];
+        testworker[0] = new Worker(30, "Tom", 'm', currentPosition);
+        Player testPlayer= new Player("Tom", 30, testworker, null );
+        testPlayer.setGod(testGod);
+        assertEquals(testGod,testPlayer.getGod());
+
+
     }
 }
--- src/test/java/Model/WorkerTest.java
+++ src/test/java/Model/WorkerTest.java
@@ -1,5 +1,6 @@
 package Model;
 
+import Exception.Worker.*;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,38 +8,92 @@ import static org.junit.Assert.*;
 public class WorkerTest {
 
     @Test
+    public void changePosition() {
+        Space currPosition = new Space(4,3,4);
+        Worker testWorker= new Worker(23, "Tom", 'm', currPosition);
+        Space newPosition = new Space(2,2,4);
+        testWorker.changePosition(newPosition);
+        assertEquals(newPosition, testWorker.getWorkerPosition());
+    }
+
+    @Test
     public void getWorkerID() {
+        Space currentPosition = new Space(4,3,4);
+        Worker testWorker = new Worker(30, "Tom", 'm', currentPosition);
+        assertEquals(30,testWorker.getWorkerID());
     }
 
     @Test
     public void getOwner() {
+        Space currentPosition = new Space(4,3,4);
+        Worker testWorker = new Worker(30, "Tom", 'm', currentPosition);
+        assertEquals("Tom",testWorker.getOwner());
+
     }
 
     @Test
     public void isWasMoved() {
+        Space currentPosition = new Space(4,3,4);
+        Worker testWorker = new Worker(30, "Tom", 'm', currentPosition);
+        testWorker.setWasMoved(true);
+        assertTrue(testWorker.isWasMoved());
     }
+
 
     @Test
     public void setWasMoved() {
+        Space currentPosition = new Space(4,3,4);
+        Worker testWorker = new Worker(30, "Tom", 'm', currentPosition);
+        testWorker.setWasMoved(true);
+        assertTrue(testWorker.isWasMoved());
     }
 
     @Test
     public void isMovedUp() {
+        Space currentPosition = new Space(4,3,4);
+        Worker testWorker = new Worker(30, "Tom", 'm', currentPosition);
+        testWorker.setMovedUp(true);
+        assertTrue(testWorker.isMovedUp());
     }
 
     @Test
     public void setMovedUp() {
+        Space currentPosition = new Space(4,3,4);
+        Worker testWorker = new Worker(30, "Tom", 'm', currentPosition);
+        testWorker.setMovedUp(true);
+        assertTrue(testWorker.isMovedUp());
     }
 
     @Test
     public void getGender() {
+        Space currentPosition = new Space(4,3,4);
+        Worker testWorker = new Worker(30, "Tom", 'm', currentPosition);
+        assertEquals('m',testWorker.getGender());
     }
 
     @Test
     public void getWorkerPosition() {
+        Space currentPosition = new Space(4,3,4);
+        Worker testWorker = new Worker(30, "Tom", 'm', currentPosition);
+        testWorker.setWorkerPosition(currentPosition);
+        assertEquals(currentPosition,testWorker.getWorkerPosition());
     }
 
     @Test
     public void setWorkerPosition() {
+        Space currentPosition = new Space(4,3,4);
+        Worker testWorker = new Worker(30, "Tom", 'm', null);
+        testWorker.setWorkerPosition(currentPosition);
+        assertEquals(currentPosition,testWorker.getWorkerPosition());
     }
+
+      /*  @Test (expected = InvalidWorkerPositionException.class)
+        public void setWorkerPositionException()  {
+        Space currentPosition = new Space(4,3,4);
+        Worker placedWorker = new Worker(30, "Tom", 'm', null);
+        placedWorker.setWorkerPosition(currentPosition);
+        Worker testWorker = new Worker(30, "Tom", 'm', null);
+        testWorker.setWorkerPosition(currentPosition);
+        assertEquals(currentPosition,testWorker.getWorkerPosition());
+    }*/
 }
